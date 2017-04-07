a38c4edb2143ae23a1e6d43a4651432ee6c20736
angular-cli <angular-cli@angular.io>
2017-04-07 15:54:04 +0200

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TreeFellingPage } from './app.po';
+
+describe('tree-felling App', function() {
+  let page: TreeFellingPage;
+
+  beforeEach(() => {
+    page = new TreeFellingPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
